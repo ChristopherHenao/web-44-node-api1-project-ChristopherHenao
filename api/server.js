8c8344979a1da2e79f0716fa738f767196d94277
ChristoherHenao <cq.henao@gmail.com>
2021-08-02 16:51:20 -0400

    post end point structure done

--- api/server.js
+++ api/server.js
@@ -1,4 +1,29 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
+const Users = require("./users")
 
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json())
+
+// | Method | URL            | Description                                                                                            |
+// | ------ | -------------- | ------------------------------------------------------------------------------------------------------ |
+// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                   |
+// | GET    | /api/users     | Returns an array users.                                                                                |
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
+// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
+// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
+
+// POST /api/users (Creates a user)
+server.post("/api/users", (req, res) => {
+    const { name, bio } = req.body
+    Users.insert({ name, bio })
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({message: err})
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}

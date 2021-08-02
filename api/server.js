cfa4a53323f3ef550c2ec5474d01f4b3a575c919
ChristoherHenao <cq.henao@gmail.com>
2021-08-02 18:12:00 -0400

    post endpoint fixed

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // BUILD YOUR SERVER HERE
-const express = require("express")
-const Users = require("./users")
+const express = require('express')
+const Users = require("./users/model")
 
 const server = express()
 
@@ -16,14 +16,39 @@ server.use(express.json())
 
 // POST /api/users (Creates a user)
 server.post("/api/users", (req, res) => {
+    // res.json('foo')
     const { name, bio } = req.body
     Users.insert({ name, bio })
     .then(user => {
-        res.status(201).json(user)
+        if(!name || !bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        } else {
+            res.status(201).json(user)
+        } 
     })
     .catch(err => {
-        res.status(500).json({message: err})
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
     })
+})
+
+// GET /api/users (returns an array of users)
+server.get("/api/users", (req, res) => {
+    res.json('foo')
+})
+
+// GET /api/users/:id (returns the user object with the specified 'id')
+server.get("/api/users", (req, res) => {
+    res.json('foo')
+})
+
+// DELETE /api/users/:id (Removes the user with the specified `id` and returns the deleted user)
+server.delete("/api/users/:id", (req, res) => {
+    res.json('foo')
+})
+
+// PUT /api/users/:id (Updates the user with the specified `id` using data from the `request body`. Returns the modified user)
+server.put("/api/users/:id", (req, res) => {
+    res.json('foo')
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
